b34cb43763f494054e1f6787e7f486b55a78031d
shehaaz <shehaaz@gmail.com>
2016-04-03 16:36:05 -0700

    encapsulated the file incrementor

--- src/main/scala/wordcounter/AkkaWordCounter.scala
+++ src/main/scala/wordcounter/AkkaWordCounter.scala
@@ -12,8 +12,9 @@ import akka.actor._
 case class ProcessStringMsg(lineNumber: Int, fileName: String, line: String, fileSender: Option[ActorRef], listener: ActorRef)
 case class StringProcessedMsg(fileSender: Option[ActorRef])
 case class CaptureStreamMsg(fileName: String, numOfWords: Int, lineNumber: Int)
-case class closeStreamMsg(totalTime: Long, fileName: String)
+case class CloseStreamMsg(totalTime: Long, fileName: String)
 case class StartProcessFileMsg()
+case class IncrementFileCounter()
 
 //A class to hold the file name and input stream
 class FileReference(val fileName: String, val stream: InputStream)
@@ -43,7 +44,7 @@ class StringCounterActor extends Actor {
 
 
 
-class RoutingActor(fileRef: FileReference, listener: ActorRef) extends Actor {
+class RoutingActor(fileRef: FileReference, listener: ActorRef, incrementor: ActorRef) extends Actor {
 
   private var running = false
   private var totalLines = 0
@@ -70,10 +71,12 @@ class RoutingActor(fileRef: FileReference, listener: ActorRef) extends Actor {
       linesProcessed += 1
 
       if (linesProcessed == totalLines) {
+        //A file has completed processing
         val stopTime = System.nanoTime()
-        listener ! closeStreamMsg(stopTime-startTime, fileName)
+        listener ! CloseStreamMsg(stopTime-startTime, fileName)
+        incrementor ! IncrementFileCounter()
         rootSender match {
-          case (Some(o)) => o ! linesProcessed // provide result to process invoker (i.e: futureResult)
+          case (Some(o)) => //o ! linesProcessed // provide result to process invoker (i.e: futureResult)
         }
       }
     }
@@ -81,7 +84,7 @@ class RoutingActor(fileRef: FileReference, listener: ActorRef) extends Actor {
   }
 }
 
-class Listener extends Actor {
+class Listener() extends Actor {
   private var totalNumberOfWords = 0
 
   def receive = {
@@ -91,11 +94,27 @@ class Listener extends Actor {
     //println(fileName + " " + "L." + lineNumber + " " + numOfWords + " words")
     //Stream results to Client
 
-    case closeStreamMsg(totalTime, fileName) =>
+    case CloseStreamMsg(totalTime, fileName) =>
       println("Stream Complete: " + fileName + " Total Number of Words: " + totalNumberOfWords +
         " Total Time: " + totalTime/1000000 + "ms")
 
     case _ => println("Error: message not recognized")
+  }
+}
+
+class Incrementor(totalNumOfFiles: Int, system: ActorSystem) extends Actor {
+
+  private var fileCounter = 0
+
+  def receive = {
+    case IncrementFileCounter() =>
+          fileCounter += 1
+          if(totalNumOfFiles == fileCounter)
+            {
+              system.terminate()
+            }
+
+    case _ => println("Error: message not recognized I")
   }
 }
 
@@ -106,7 +125,6 @@ object AkkaWordCounter extends App {
   import akka.pattern.ask
   import akka.dispatch.ExecutionContexts._
 
-  private var fileCounter = 0
   private var numberOfFiles = 0
 
   override def main(args: Array[String]) {
@@ -122,11 +140,12 @@ object AkkaWordCounter extends App {
       * The result of the operation is again Unit; no list of results is assembled.
       */
     numberOfFiles = files.length
-    files.foreach(x => initActorSystem(x, system))
+    val incrementor = system.actorOf(Props(new Incrementor(numberOfFiles, system)))
+    files.foreach(x => initActorSystem(x, system, incrementor))
 
   }
 
-  def initActorSystem(fileName: String, system: ActorSystem): Unit = {
+  def initActorSystem(fileName: String, system: ActorSystem, incrementor: ActorRef): Unit = {
     //Fixing bug from original code: https://www.toptal.com/scala/concurrency-and-fault-tolerance-made-easy-an-intro-to-akka#comment-1776147740
     implicit val executionContext = global
     // create the result listener, which will print the result. Didn't give a name, so it will be unique every time
@@ -134,20 +153,42 @@ object AkkaWordCounter extends App {
     //Load from /resources folder: http://stackoverflow.com/questions/27360977/how-to-read-files-from-resources-folder-in-scala
     val stream : InputStream = getClass.getResourceAsStream("/" + fileName)
     //Again Didn't give the actor a name, so it will be unique every time
-    val routingActor = system.actorOf(Props(new RoutingActor(new FileReference(fileName, stream), listener)))
+    val routingActor = system.actorOf(Props(new RoutingActor(new FileReference(fileName, stream), listener, incrementor)))
     implicit val timeout = Timeout(5 seconds)
-    //When the future returns after all the work is complete for each file
-    val futureResult = routingActor ? StartProcessFileMsg()
-    futureResult.map { result =>
-      //println("Number of lines processed in " + fileName + ": " + result)
-      //Increment the file counter when a result is received
-      fileCounter += 1
-      if(fileCounter == numberOfFiles)
-        {
-          //Terminate Actor System when all the files have been processed
-          system.terminate()
-        }
-    }
+
+    /**
+      * Messages are sent to an Actor through one of the following methods.
+      * ! means “fire-and-forget”, e.g. send a message asynchronously and return immediately. Also known as tell.
+      * ? sends a message asynchronously and returns a Future representing a possible reply. Also known as ask.
+      */
+
+    /**
+      * Option One: Do a Ask call
+      * http://doc.akka.io/docs/akka/current/scala/futures.html
+      * When the future returns after all the work is complete for each file
+      */
+//    val futureResult = routingActor ? StartProcessFileMsg()
+//    futureResult.map { result =>
+//      //"result" can be also be an object. You simply have to do result.asInstanceOf[ObjectName]
+//      //println("Number of lines processed in " + fileName + ": " + result)
+//    }
+
+    /**
+      * Option two: Do a Tell call
+      * http://doc.akka.io/docs/akka/2.4.1/scala/actors.html#Send_messages
+      * Except from "Effective Akka"
+      * Tell, don’t ask
+      * A best practice for actor development is to avoid ask: use fire and forget messages, and be prepared to handle responses.
+      * This is simpler and more expressive than using futures and having to map over composed responses,
+      * but I’m also only using one asynchronous threaded resource (the anonymous actor I created) as opposed to multiple futures for
+      * the sends and the myriad futures returned within a composed for comprehension to handle the results. Always try to focus on tell over ask.
+      * Truly fault-resilient systems send messages in fire and forget fashion and prepare to receive the expected response.
+      * They do not make assumptions that sending once means that the message was definitely received and is being handled, since anything can happen in between.
+      * As such, we should schedule a task to continually resend that message a pre-defined number of times in some acceptable duration.
+      * If no expected response is received within that timeframe, we should be able to handle or escalate the failure.
+      *
+      */
+     routingActor ! StartProcessFileMsg()
 
   }
 
